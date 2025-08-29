b4872e6913eda743e5f067ff180f9e5253735e7b
Ramel Gulane <pitad21.gulaneramel@gmail.com>
2025-08-29 08:21:14 +0800

    Update

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace BorrowingSystem.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: /Login
+        public IActionResult Index()
+        {
+            return View();  // looks for Views/Login/Index.cshtml
+        }
+    }
+}
